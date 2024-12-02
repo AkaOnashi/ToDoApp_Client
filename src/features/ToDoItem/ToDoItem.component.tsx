5cecfb0525ae177f06f12d1ff76b3f48b4efbada
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2024-12-02 10:16:49 +0200

    Added new funcs.

--- src/features/ToDoItem/ToDoItem.component.tsx
+++ src/features/ToDoItem/ToDoItem.component.tsx
@@ -1,5 +1,9 @@
+import React, { useState} from 'react';     
 import './ToDoItem.styles.scss';
 import { Button } from "antd";
+import { Checkbox } from 'antd';
+import type { CheckboxProps } from 'antd';
+
 
 interface ToDoItemProps {
     task: string; 
@@ -7,13 +11,38 @@ interface ToDoItemProps {
     index: number; 
     deleteTask: (index: number) => void; 
     editTask: (index: number) => void; 
+
+    dragStartHandler: (e: React.DragEvent, index: number) => void;
+    dragOverHandler: (e: React.DragEvent) => void;
+    dragEndHandler: (e: React.DragEvent) => void;
+    dropHandler: (e: React.DragEvent, index: number) => void;
 }
 
-const ToDoItem: React.FC<ToDoItemProps> = ({task, status, index, deleteTask, editTask}) => {
+
+
+const ToDoItem: React.FC<ToDoItemProps> = ({task, status, index, deleteTask, 
+    editTask, dragStartHandler, dragOverHandler, dragEndHandler, dropHandler}) => {
+    
+    const [isCompleted, setIsCompleted] = useState(false);
+    
+    const onChange: CheckboxProps['onChange'] = (e) => {
+        setIsCompleted(e.target.checked);
+    };
+
     return (
-        <li>
-            <span className="task-name">{task}</span>
+        <li
+            className="todo-item"
+            draggable="true"
+            onDragStart={(e) => dragStartHandler(e, index)}
+            onDragOver={dragOverHandler}
+            onDragEnd={dragEndHandler}
+            onDrop={(e) => dropHandler(e, index)}>
+
+            <Checkbox checked={isCompleted} onChange={onChange}></Checkbox>  
+
+            <span className={`task-name ${isCompleted ? 'completed' : ''}`}>{task}</span>
             <span className="status-text">ToDo</span>
+            
             <Button 
                 className="edit-button"
                 color="default" 
--- src/features/ToDoList/ToDoList.component.tsx
+++ src/features/ToDoList/ToDoList.component.tsx
@@ -3,11 +3,14 @@ import './ToDoList.styles.scss';
 import ToDoItem from '../ToDoItem/ToDoItem.component';
 import { Button } from "antd";
 import { Input } from 'antd';
+import FilterDropdown from '../../app/components/FilterDropdown/FilterDropdown.component';
+
 
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Watch the video", "Visit the granny"]);
+    const [tasks, setTasks] = useState(["Watch the video", "Visit the granny", "To be a chill guy"]);
     const[newTask, setNewTask] = useState("");
     const [status, setNewStatus] = useState("ToDo");
+    const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
 
     function handleInputChange(event:  React.ChangeEvent<HTMLInputElement>) {
         setNewTask(event.target.value);
@@ -30,6 +33,29 @@ function ToDoList() {
 
     }
 
+    function dragStartHandler(e: React.DragEvent, index: number) {
+        setDraggedIndex(index);
+    }
+
+    function dragOverHandler(e: React.DragEvent) {
+        e.preventDefault();  
+    }
+
+    function dragEndHandler(e: React.DragEvent) {
+        setDraggedIndex(null);  
+    }
+
+    function dropHandler(e: React.DragEvent, index: number) {
+        e.preventDefault();
+        if (draggedIndex !== null) {
+            const updatedTasks = [...tasks];
+            const draggedTask = updatedTasks[draggedIndex];
+            updatedTasks.splice(draggedIndex, 1);
+            updatedTasks.splice(index, 0, draggedTask);  
+            setTasks(updatedTasks);  
+        }
+    }
+
     return(
         <div>
             <div className="input-box">
@@ -45,7 +71,14 @@ function ToDoList() {
                 onClick={addTask}>
                 Add Task        
             </Button>
+            <span className="filter-button">
+                <FilterDropdown/>
+            </span>
+            
             </div>
+
+            
+
             <ol>
                 {tasks.map((task, index) => 
                     <ToDoItem 
@@ -54,7 +87,12 @@ function ToDoList() {
                         status={status}
                         index={index}
                         deleteTask={deleteTask}
-                        editTask={editTask} />
+                        editTask={editTask}
+                        
+                        dragStartHandler={dragStartHandler}
+                        dragOverHandler={dragOverHandler}
+                        dragEndHandler={dragEndHandler}
+                        dropHandler={dropHandler}/>
                 )}
             </ol>
         </div>);
